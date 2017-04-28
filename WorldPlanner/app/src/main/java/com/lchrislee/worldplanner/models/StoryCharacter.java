e8df2714aceb74abb4b7ded24fcec94b03314493
Chris Lee <c.lee.840@gmail.com>
2017-04-27 22:20:02 -0400

    Added physical features for StoryCharacter.

--- WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/models/StoryCharacter.java
+++ WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/models/StoryCharacter.java
@@ -9,14 +9,47 @@ import java.io.Serializable;
 
 public class StoryCharacter extends SugarRecord implements Serializable, StoryElement{
 
+    private StoryWorld world;
+
+    // Basic features.
     private String name;
     private String description;
     private String nickname;
     private String gender;
     private String imagePath; // In SugarORM: image_path;
     private int age;
+    private int height;
+    private int weight;
 
-    private StoryWorld world;
+    // Head features.
+    private String hair;
+    private String eyes;
+    private String ears;
+    private String nose;
+    private String mouth;
+    private String neck;
+
+    // Torso features.
+    private String shoulders;
+    private String chest;
+    private String stomach;
+    private String back;
+
+    // Arm features
+    private String upperLeftArm;
+    private String upperRightArm;
+    private String lowerLeftArm;
+    private String lowerRightArm;
+    private String handLeft;
+    private String handRight;
+
+    // Leg features
+    private String upperLeftLeg;
+    private String upperRightLeg;
+    private String lowerLeftLeg;
+    private String lowerRightLeg;
+    private String footLeft;
+    private String footRight;
 
     public StoryCharacter() {
         name = "";
@@ -87,5 +120,197 @@ public class StoryCharacter extends SugarRecord implements Serializable, StoryEl
 
     public StoryWorld getWorld() {
         return world;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getHair() {
+        return hair;
+    }
+
+    public void setHair(String hair) {
+        this.hair = hair;
+    }
+
+    public String getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(String eyes) {
+        this.eyes = eyes;
+    }
+
+    public String getEars() {
+        return ears;
+    }
+
+    public void setEars(String ears) {
+        this.ears = ears;
+    }
+
+    public String getNose() {
+        return nose;
+    }
+
+    public void setNose(String nose) {
+        this.nose = nose;
+    }
+
+    public String getMouth() {
+        return mouth;
+    }
+
+    public void setMouth(String mouth) {
+        this.mouth = mouth;
+    }
+
+    public String getNeck() {
+        return neck;
+    }
+
+    public void setNeck(String neck) {
+        this.neck = neck;
+    }
+
+    public String getShoulders() {
+        return shoulders;
+    }
+
+    public void setShoulders(String shoulders) {
+        this.shoulders = shoulders;
+    }
+
+    public String getChest() {
+        return chest;
+    }
+
+    public void setChest(String chest) {
+        this.chest = chest;
+    }
+
+    public String getStomach() {
+        return stomach;
+    }
+
+    public void setStomach(String stomach) {
+        this.stomach = stomach;
+    }
+
+    public String getBack() {
+        return back;
+    }
+
+    public void setBack(String back) {
+        this.back = back;
+    }
+
+    public String getUpperLeftArm() {
+        return upperLeftArm;
+    }
+
+    public void setUpperLeftArm(String upperLeftArm) {
+        this.upperLeftArm = upperLeftArm;
+    }
+
+    public String getUpperRightArm() {
+        return upperRightArm;
+    }
+
+    public void setUpperRightArm(String upperRightArm) {
+        this.upperRightArm = upperRightArm;
+    }
+
+    public String getLowerLeftArm() {
+        return lowerLeftArm;
+    }
+
+    public void setLowerLeftArm(String lowerLeftArm) {
+        this.lowerLeftArm = lowerLeftArm;
+    }
+
+    public String getLowerRightArm() {
+        return lowerRightArm;
+    }
+
+    public void setLowerRightArm(String lowerRightArm) {
+        this.lowerRightArm = lowerRightArm;
+    }
+
+    public String getHandLeft() {
+        return handLeft;
+    }
+
+    public void setHandLeft(String handLeft) {
+        this.handLeft = handLeft;
+    }
+
+    public String getHandRight() {
+        return handRight;
+    }
+
+    public void setHandRight(String handRight) {
+        this.handRight = handRight;
+    }
+
+    public String getUpperLeftLeg() {
+        return upperLeftLeg;
+    }
+
+    public void setUpperLeftLeg(String upperLeftLeg) {
+        this.upperLeftLeg = upperLeftLeg;
+    }
+
+    public String getUpperRightLeg() {
+        return upperRightLeg;
+    }
+
+    public void setUpperRightLeg(String upperRightLeg) {
+        this.upperRightLeg = upperRightLeg;
+    }
+
+    public String getLowerLeftLeg() {
+        return lowerLeftLeg;
+    }
+
+    public void setLowerLeftLeg(String lowerLeftLeg) {
+        this.lowerLeftLeg = lowerLeftLeg;
+    }
+
+    public String getLowerRightLeg() {
+        return lowerRightLeg;
+    }
+
+    public void setLowerRightLeg(String lowerRightLeg) {
+        this.lowerRightLeg = lowerRightLeg;
+    }
+
+    public String getFootLeft() {
+        return footLeft;
+    }
+
+    public void setFootLeft(String footLeft) {
+        this.footLeft = footLeft;
+    }
+
+    public String getFootRight() {
+        return footRight;
+    }
+
+    public void setFootRight(String footRight) {
+        this.footRight = footRight;
     }
 }
