923c58d8d153c48912e1974dfbee4a5ac6c664de
Chris Lee <c.lee.840@gmail.com>
2018-01-13 14:34:41 -0500

    Moved visual changes out of presenter. Added a header to navigation drawer.

--- WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/activities/HomeActivity.java
+++ WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/activities/HomeActivity.java
@@ -1,16 +1,23 @@
 package com.lchrislee.worldplanner.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.lchrislee.worldplanner.R;
 import com.lchrislee.worldplanner.presenters.HomePresenter;
@@ -23,7 +30,7 @@ import org.androidannotations.annotations.ViewById;
  * The main entry point. The user can view all their worlds and all entities in their worlds.
  */
 @EActivity(R.layout.activity_home)
-public class HomeActivity extends BaseFragmentActivity
+public class HomeActivity extends BaseFragmentActivity implements HomePresenter.HomeDelegate
 {
     private static final String LOG_TAG = HomeActivity.class.getSimpleName();
 
@@ -37,12 +44,23 @@ public class HomeActivity extends BaseFragmentActivity
 
     @ViewById TabLayout homeTabs;
 
+    private ImageView headerImage;
+
+    private TextView headerTitle;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private FragmentStatePagerAdapter mPagerAdapter;
+
+    private Fragment[] fragments = new Fragment[10];
+
     private HomePresenter mHomePresenter;
 
     @Override
     protected void onPostCreate (@Nullable final Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         initializeNavigation();
+        initializeTabs();
     }
 
     private void initializeNavigation () {
@@ -52,6 +70,41 @@ public class HomeActivity extends BaseFragmentActivity
             actionBar.setTitle(R.string.app_name);
             actionBar.setHomeButtonEnabled(true);
         }
+        mDrawerToggle = new ActionBarDrawerToggle(this, homeDrawer, toolbar, 0, 0);
+        homeDrawer.addDrawerListener(mDrawerToggle);
+        final View headerView = homeNavigation.inflateHeaderView(R.layout.layout_home_navigation_header);
+        headerImage = headerView.findViewById(R.id.homeNavigationHeaderImage);
+        headerTitle = headerView.findViewById(R.id.homeNavigationHeaderTitle);
+        mDrawerToggle.syncState();
+    }
+
+    private void initializeTabs() {
+        for (int i = 0; i < fragments.length; ++i) {
+            fragments[i] = new Fragment();
+        }
+        mPagerAdapter = new FragmentStatePagerAdapter(getSupportFragmentManager())
+        {
+            @NonNull
+            @Override
+            public Fragment getItem (final int position)
+            {
+                return fragments[position];
+            }
+
+            @Override
+            public int getCount ()
+            {
+                return fragments.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle (final int position)
+            {
+                return "User world " + String.valueOf(position);
+            }
+        };
+        homeViewPager.setAdapter(mPagerAdapter);
+        homeTabs.setupWithViewPager(homeViewPager, true);
     }
 
     @Override
@@ -65,9 +118,16 @@ public class HomeActivity extends BaseFragmentActivity
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onConfigurationChanged (Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
     @AfterViews
     protected void onViewLoaded() {
-        mHomePresenter = new HomePresenter(homeNavigation, homeDrawer, toolbar, homeViewPager, homeTabs);
+        mHomePresenter = new HomePresenter();
         mHomePresenter.attach(this);
     }
 }
--- WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/presenters/HomePresenter.java
+++ WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/presenters/HomePresenter.java
@@ -1,15 +1,7 @@
 package com.lchrislee.worldplanner.presenters;
 
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 /**
  * The main controller for {@link com.lchrislee.worldplanner.activities.HomeActivity}. This determines
@@ -19,68 +11,15 @@ public class HomePresenter
 {
     private static final String LOG_TAG = HomePresenter.class.getSimpleName();
 
-    private NavigationView homeNavigation;
+    private HomeDelegate mDelegate;
 
-    private DrawerLayout homeDrawer;
+    public interface HomeDelegate {
 
-    private Toolbar toolbar;
-
-    private ViewPager homeViewPager;
-
-    private TabLayout homeTabs;
-
-    private ActionBarDrawerToggle mDrawerToggle;
-
-    private FragmentStatePagerAdapter mPagerAdapter;
-
-    private Fragment[] fragments = new Fragment[10];
-
-    public HomePresenter (@NonNull final NavigationView homeNavigation,
-                          @NonNull final DrawerLayout homeDrawer,
-                          @NonNull final Toolbar toolbar,
-                          @NonNull final ViewPager homeViewPager,
-                          @NonNull final TabLayout homeTabs)
-    {
-        this.homeNavigation = homeNavigation;
-        this.homeDrawer = homeDrawer;
-        this.toolbar = toolbar;
-        this.homeViewPager = homeViewPager;
-        this.homeTabs = homeTabs;
     }
 
-    public void attach(@NonNull final AppCompatActivity activity) {
-        mDrawerToggle = new ActionBarDrawerToggle(activity, homeDrawer, toolbar, 0, 0);
-        homeDrawer.addDrawerListener(mDrawerToggle);
-        mDrawerToggle.syncState();
-        initializeTabs(activity);
+    public void attach(@NonNull final HomeDelegate delegate) {
+        Log.d(LOG_TAG, "Attached home presenter.");
+        mDelegate = delegate;
     }
 
-    private void initializeTabs(@NonNull final AppCompatActivity activity) {
-        for (int i = 0; i < fragments.length; ++i) {
-            fragments[i] = new Fragment();
-        }
-        mPagerAdapter = new FragmentStatePagerAdapter(activity.getSupportFragmentManager())
-        {
-            @NonNull
-            @Override
-            public Fragment getItem (final int position)
-            {
-                return fragments[position];
-            }
-
-            @Override
-            public int getCount ()
-            {
-                return fragments.length;
-            }
-
-            @Override
-            public CharSequence getPageTitle (final int position)
-            {
-                return "User world " + String.valueOf(position);
-            }
-        };
-        homeViewPager.setAdapter(mPagerAdapter);
-        homeTabs.setupWithViewPager(homeViewPager, true);
-    }
 }
