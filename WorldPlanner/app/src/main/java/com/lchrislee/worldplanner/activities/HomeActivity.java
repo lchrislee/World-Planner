314f48023e8540eed710327dae6753ef8efbf699
Chris Lee <c.lee.840@gmail.com>
2018-01-13 23:17:06 -0500

    Made the Navigation Drawer more clear and added most of the options.

--- WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/activities/HomeActivity.java
+++ WorldPlanner/app/src/main/java/com/lchrislee/worldplanner/activities/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -13,21 +14,24 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lchrislee.worldplanner.R;
 import com.lchrislee.worldplanner.presenters.HomePresenter;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 /**
- * The main entry point. The user can view all their worlds and all entities in their worlds.
+ * The main place for users. The user can view all their worlds and all entities in their worlds.
  */
 @EActivity(R.layout.activity_home)
 public class HomeActivity extends BaseFragmentActivity implements HomePresenter.HomeDelegate
@@ -59,19 +63,26 @@ public class HomeActivity extends BaseFragmentActivity implements HomePresenter.
     @Override
     protected void onPostCreate (@Nullable final Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+        initializeToolbar();
         initializeNavigation();
         initializeTabs();
     }
 
-    private void initializeNavigation () {
+    private void initializeToolbar() {
         setSupportActionBar(toolbar);
         final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setTitle(R.string.app_name);
             actionBar.setHomeButtonEnabled(true);
         }
+    }
+
+    private void initializeNavigation () {
         mDrawerToggle = new ActionBarDrawerToggle(this, homeDrawer, toolbar, 0, 0);
         homeDrawer.addDrawerListener(mDrawerToggle);
+        homeNavigation.setNavigationItemSelectedListener(this::onOptionsItemSelected);
+        final NavigationMenuView homeNavigationMenu = (NavigationMenuView) homeNavigation.getChildAt(0);
+        homeNavigationMenu.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         final View headerView = homeNavigation.inflateHeaderView(R.layout.layout_home_navigation_header);
         headerImage = headerView.findViewById(R.id.homeNavigationHeaderImage);
         headerTitle = headerView.findViewById(R.id.homeNavigationHeaderTitle);
@@ -114,8 +125,18 @@ public class HomeActivity extends BaseFragmentActivity implements HomePresenter.
             case android.R.id.home:
                 homeDrawer.openDrawer(GravityCompat.START);
                 return true;
+            case R.id.home_navigation_jump:
+                Toast.makeText(this, "JUMP", Toast.LENGTH_SHORT).show();
+                homeDrawer.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.home_navigation_new_world:
+                Toast.makeText(this, "NEW WORLD", Toast.LENGTH_SHORT).show();
+                homeDrawer.closeDrawer(GravityCompat.START);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @Override
@@ -129,5 +150,23 @@ public class HomeActivity extends BaseFragmentActivity implements HomePresenter.
     protected void onViewLoaded() {
         mHomePresenter = new HomePresenter();
         mHomePresenter.attach(this);
+    }
+
+    @Click({R.id.homeSettings})
+    protected void onHomeSettingsClicked() {
+        Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        homeDrawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Click({R.id.homeFeedback})
+    protected void onHomeFeedbackClickeD() {
+        Toast.makeText(this, "Feedback", Toast.LENGTH_SHORT).show();
+        homeDrawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Click({R.id.homeAbout})
+    protected void onHomeAboutClicked() {
+        Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+        homeDrawer.closeDrawer(GravityCompat.START);
     }
 }
